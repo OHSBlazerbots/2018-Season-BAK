c122aa34912fe2d1b4ba9ddc24525a12fb9e8c62
blazerbots <ohsblazerbots@gmail.com>
2018-01-15 16:01:51 -0700

    Added in the base classes

--- src/org/usfirst/frc/team3807/robot/Robot.java
+++ src/org/usfirst/frc/team3807/robot/Robot.java
@@ -1,5 +1,40 @@
 package org.usfirst.frc.team3807.robot;
 
-public class Robot {
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+public class Robot extends IterativeRobot{
+	public static OI oi;
+	
+	@Override
+	public void robotInit(){
+		oi = new OI();
+		CommandBase.init();
+	}
+	
+	@Override
+	public void autonomousInit(){
+		
+	}
+	
+	@Override
+	public void autonomousPeriodic(){
+		
+	}
+	
+	@Override
+	public void teleopInit(){
+		
+	}
+	
+	@Override
+	public void teleopPeriodic(){
+		
+	}
+	
+	@Override
+	public void testPeriodic(){
+		
+	}
 
 }
